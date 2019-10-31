8d7a699a7a2ac324ca2a6695cde86f6e9758341e
tong.luo <luotong@leyou.com>
2019-10-31 18:10:30 +0800

    add

--- demo/src/main/java/com/lt/demo/DemoApplication.java
+++ demo/src/main/java/com/lt/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.lt.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author
  */
 @SpringBootApplication
-@MapperScan("com.*")
 public class DemoApplication {
 
     public static void main(String[] args) {

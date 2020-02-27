5c1b86e229b1eebca9fb9f95b60890f923ec819e
tong.luo <luotong@leyou.com>
2020-02-27 14:37:55 +0800

    add new model

---
+++ alidemo/src/test/java/com/lt/alidemo/AlidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lt.alidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlidemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

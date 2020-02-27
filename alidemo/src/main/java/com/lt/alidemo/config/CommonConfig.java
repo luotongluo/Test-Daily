09711e4d2e6a1456d767b581a4dfe5812cec7e08
tong.luo <luotong@leyou.com>
2020-02-27 15:42:44 +0800

    test ali open api

--- alidemo/src/main/java/com/lt/alidemo/config/CommonConfig.java
+++ alidemo/src/main/java/com/lt/alidemo/config/CommonConfig.java
@@ -10,17 +10,39 @@ import org.springframework.stereotype.Component;
  * @Description: 公共的配置类
  * @Version 1.0
  */
-@RefreshScope
 @Component
+@RefreshScope
 public class CommonConfig {
-    @Value("${url.appkey}")
-    private String appkey;
+    @Value("${url.yiyuan.appkey}")
+    private String yiyuanappkey;
 
-    public String getAppkey() {
-        return appkey;
+    @Value("${url.yiyuan.AppSecret}")
+    private String yiyuanAppSecret;
+
+    @Value("${url.yiyuan.AppCode}")
+    private String yiyuanAppCode;
+
+    public String getYiyuanappkey() {
+        return yiyuanappkey;
     }
 
-    public void setAppkey(String appkey) {
-        this.appkey = appkey;
+    public void setYiyuanappkey(String yiyuanappkey) {
+        this.yiyuanappkey = yiyuanappkey;
+    }
+
+    public String getYiyuanAppSecret() {
+        return yiyuanAppSecret;
+    }
+
+    public void setYiyuanAppSecret(String yiyuanAppSecret) {
+        this.yiyuanAppSecret = yiyuanAppSecret;
+    }
+
+    public String getYiyuanAppCode() {
+        return yiyuanAppCode;
+    }
+
+    public void setYiyuanAppCode(String yiyuanAppCode) {
+        this.yiyuanAppCode = yiyuanAppCode;
     }
 }

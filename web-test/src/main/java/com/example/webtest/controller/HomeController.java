d4c839addc921f673ff41eb54fe50052466b6367
tong.luo <luotong@leyou.com>
2020-03-31 13:40:38 +0800

    1

--- web-test/src/main/java/com/example/webtest/controller/HomeController.java
+++ web-test/src/main/java/com/example/webtest/controller/HomeController.java
@@ -27,6 +27,11 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String home() {
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
         return "home";
     }
 

5e0b634e672a7a272b7faced32ff461ea7a74a4e
tong.luo <luotong@leyou.com>
2020-04-01 13:15:37 +0800

    1

---
+++ web-test/src/main/java/com/example/webtest/pojo/ApolloElectronicInvoiceShop.java
@@ -0,0 +1,85 @@
+package com.example.webtest.pojo;
+
+import java.util.Date;
+
+public class ApolloElectronicInvoiceShop {
+    private Integer id;
+
+    private String shopName;
+
+    private String identificationNumber;
+
+    private Short type;
+
+    private String placeCode;
+
+    private Short yn;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public String getPlaceCode() {
+        return placeCode;
+    }
+
+    public void setPlaceCode(String placeCode) {
+        this.placeCode = placeCode;
+    }
+
+    public Short getYn() {
+        return yn;
+    }
+
+    public void setYn(Short yn) {
+        this.yn = yn;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
